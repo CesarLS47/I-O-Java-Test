29d75dab004060f4003dd7c59bbaf65351bce328
Cesar <cresar.47@gmail.com>
2022-08-24 18:34:23 +0200

    Refactor de nombres

---
+++ src/teststreams/common/Main.java
@@ -0,0 +1,33 @@
+package teststreams.common;
+
+import teststreams.escribirPorCaracteres.Escribir_Fichero;
+import teststreams.leyendoPorCaracteres.Acceso_Fichero;
+import teststreams.serializacionObjetos.ObjetoParaSerializar;
+import teststreams.serializacionObjetos.Serializador;
+
+public class Main {
+    public static void main(String[] args) {
+        /*Leer un archivo como caracteres*/
+        //Acceso_Fichero leyendo = new Acceso_Fichero();
+        //leyendo.lee();
+
+        //Escribir en un archivo como caracteres
+        Escribir_Fichero escribiendo = new Escribir_Fichero();
+        escribiendo.escribe();
+
+
+        /* Serializacion */
+        // Objeto ejemplo para serializar, importante que el objeto implemente la interfaz "Serializable"
+        ObjetoParaSerializar objetoPruebaParaSerializar = new ObjetoParaSerializar();
+
+        // Incluye los metodos para serializar un objeto a un archivo, y para des-serializarlo
+        Serializador serializador= new Serializador();
+
+        serializador.SerializarObjetoToArchivo(objetoPruebaParaSerializar);
+        ObjetoParaSerializar ObjetoPruebaDeserializado = serializador.DesSerializarArchivoToObjeto();
+
+        System.out.println("\n Los objetos coinciden en su datos? " + ObjetoPruebaDeserializado.equals(objetoPruebaParaSerializar));
+        System.out.println(ObjetoPruebaDeserializado);
+
+    }
+}
